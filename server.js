a7558066b0a79bb53de3ff51689c634b9d187d2d
Edward Anthony Escudero Bowie <57167218+Kayeddy@users.noreply.github.com>
2023-03-20 12:55:37 -0500

    Model set up and CRUD creation

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Student = require("./models/student");
 
 const app = express();
 const dbUri =
@@ -8,6 +9,7 @@ const dbUri =
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+app.use(express.json());
 
 (async () => {
   try {
@@ -20,3 +22,59 @@ app.listen(3000, () => {
     console.log(error);
   }
 })();
+
+// Routes
+
+//Get all students
+app.get("/Student", async (req, res) => {
+  try {
+    const students = await Student.find({});
+    res.status(200).json(students);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Get one student by id
+app.get("/Student/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const student = await Student.findById(id);
+    res.status(200).json(student);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Create a new student
+app.post("/Student", async (req, res) => {
+  try {
+    const student = await Student.create(req.body);
+    res.status(200).json(student);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Update a student by id
+app.put("/Student/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const student = await Student.findByIdAndUpdate(id, req.body);
+    res.status(200).json(student);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    res.status(404).json({ message: "Student not found" });
+  }
+});
+
+// Delete a student by id
+app.delete("/Student/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const student = await Student.findByIdAndDelete(id);
+    res.status(200).json(student);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
