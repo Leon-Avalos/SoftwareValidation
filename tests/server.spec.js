fd1f2ea307655c4949104511402f3399de93c83d
Leon Avalos <heiko.avalos@gmail.com>
2023-03-21 00:43:52 -0500

    Create test for student by id and modify packagejson

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -8,3 +8,14 @@ describe("GET /Student", () => {
     expect(response.status).toBe(200);
   });
 });
+
+
+// Test 2 - Get Student name
+describe('GET /Student', () => {
+  test('should return a student called John Doe when find by id', async () => {
+    const student_id = "64193e7f048b526d72240ff8"
+    const student_name = "John Doe"
+    const response = await request(server).get(`/Student/${student_id}`).send()
+    expect(response.body.name).toBe(student_name);
+  });
+});
